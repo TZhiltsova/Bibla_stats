6d87c362f9abede7d2e10929123e0bc042a5b9ad
TZhiltsova <themoonstuck@mail.ru>
2023-03-16 13:51:31 +0300

    start

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Библа')
+import pandas as pd
+
+
+xl = pd.read_excel(
+    io='Статистика библа 21-23.xlsx',
+    sheet_name=['Библиотека 2023'],
+    usecols='B:I',
+    header=2
+)
+for key in xl.keys():
+    print(key)
+    '''
+xl = pd.DataFrame(xl, index=[181])
+
+print(xl.iloc[:, 6])
+'''
